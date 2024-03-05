1ed1c9667b1248a6aa5a10c7a0ef83cc58aab434
Alexandre Siljegovic <130472981+AlexandreSiljegovic@users.noreply.github.com>
2024-03-05 22:38:03 +0100

    slt

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,5 @@
 
+
 const app = new Framework7({
     // App root element
     el: '#app',
@@ -14,9 +15,49 @@ const app = new Framework7({
         path: '/about/',
         url: 'about.html',
       },
+      {
+        path: '/messages/',
+        url: '/messages/',
+        componentUrl: './pages/messages.html',
+      },
+      {
+        path: '/identification/',
+        url: '/identification/',
+        componentUrl: './pages/identification.html',
+      }
     ],
     // ... other parameters
   });
 
   
 const mainView = app.views.create('.view-main');
+
+
+
+
+let getData = async function () {
+  let data = await fetch('https://httpbin.org/ip')
+  return data.json()
+}
+
+
+
+let renderData = async function () {
+  let data = await getData()
+  console.log(data)
+}
+
+renderData()
+
+
+let workerContainerInstance = navigator.serviceWorker;
+
+if ('serviceWorker' in navigator) {
+  workerContainerInstance.register('./sw.js').then(function (registration) {
+    console.log('Service Worker registered with scope:', registration.scope);
+  }).catch(function (error) {
+    console.log('Service Worker registration failed:', error);
+  });
+} else {
+  console.log('Service Workers are not supported in this browser');
+}
